40c45219a9ac2e08fa5ddf851caf087ebacc393e
Alexander Robson <arobson@kth.se>
2023-01-17 14:01:07 +0100

    New subfolder for lab 1

--- Labb1/uppgift8.m
+++ Labb1/uppgift8.m
@@ -7,9 +7,9 @@ N = 1000; % antal steg
 implicitValue = uppgift8_implicit(N);
 
 for iter=1:maxiter
-    x(iter) = uppgift8_fixpunkt(iter, N);
+    x(iter) = uppgift8_fixpunkt(iter, N);)
 end
 
 y = abs(x - implicitValue);
 
-semilogy(iterationv, y)
+semilogy(iterationv, y
