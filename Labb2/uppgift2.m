5f87316199e16535afd17909d29e55302df877e8
jarleriksson <jarler@kth.se>
2023-01-23 18:04:13 +0100

    a
    s

--- Labb2/uppgift2.m
+++ Labb2/uppgift2.m
@@ -15,3 +15,25 @@ ymq = b*xmq + a ;
 hold 
 plot(xmq,ymq);
 plot(td , yd);
+
+%%
+% Polynominterpolation med Newtons ansats
+clear
+% Datapunkter att interpolera
+x = [0 0.5 1 1.5 2 2.99 3]';
+y = [0 0.52 1.09 1.75 2.45 3.5 4]';
+
+% Skapa matrisen (se slides)
+B = [ones(size(x)), x-x(1), (x-x(1)).*(x-x(2)), (x-x(1)).*(x-x(2)).*(x-x(3))];
+
+% Lös
+a = B\y;
+
+% Polynomet
+p = @(t) a(1) + a(2)*(t-x(1)) + a(3)*(t-x(1)).*(t-x(2)) + a(4)*(t-x(1)).*(t-x(2)).*(t-x(3));
+
+% Plotta datapunkter och polynom
+clf
+plot(x, y, 'o')
+hold on
+fplot(p, [1, 4])
