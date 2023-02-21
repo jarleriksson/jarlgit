182f4c96ab56d0e78bb5cf8dc560b478b6972366
jarleriksson <jarler@kth.se>
2023-02-21 18:10:44 +0100

    j
    jgv

--- labb3/fix_punkt.m
+++ labb3/fix_punkt.m
@@ -1,17 +1,18 @@
 function acc = fix_punkt(q, p, h)
-itr_max = 20;
-itr = 0;
-tlr = 10^-10;
-diff = 100;
-test =[q(1),q(2),p(1),p(2)];
+    itr_max = 50;
+    itr = 0;
+    tlr = 10^-10;
+    diff = 100;
+    test =[q(1),q(2),p(1),p(2)];
 
-while diff > tlr  && itr < itr_max
-   acc = acceleration([test(1),test(2)]);
-   v = test;
-   test = [q(1) + h*test(3), q(2) + h*test(4), p(1) + h*acc(1), p(2)+ h*acc(2)];
-   
-   diff = norm(v - test);
-   itr = itr + 1;
+    while diff > tlr  && itr < itr_max
+       acc = acceleration([test(1),test(2)]);
+       v = test;
+       test = [v(1) + h*test(3), v(2) + h*test(4), v(3) + h*acc(1), v(4) + h*acc(2)];
+       
+       diff = norm(v - test);
+       itr = itr + 1;
+
+    end
 
 end
-
